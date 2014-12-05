07bd26d7f29f55a967998747a26943218f7b7172
Mike Wang <yellowy4321@gmail.com>
2014-12-05 12:06:07 -0800

    Deeper traversal via assert

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ extern crate rustc;
 use syntax::codemap::{DUMMY_SP, Span};
 use syntax::parse::{mod, token, lexer};
 use syntax::parse::lexer::Reader;
-use syntax::ast::{mod, TokenTree};
+use syntax::ast::{mod, TokenTree, Expr};
 use syntax::ptr::P;
 use syntax::ext::base::{ExtCtxt, MacResult, MacExpr};
 use syntax::ext::build::AstBuilder;
@@ -21,6 +21,136 @@ fn expand_assert_ng(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
 fn expand_debug_assert_ng(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
         -> Box<MacResult + 'static> {
     expand_assert_ng_(cx, sp, args, true)
+}
+
+fn expand_assert_ng_helper(cx: &mut ExtCtxt, expr : &P<Expr>) -> P<Expr> {
+    let cm = &cx.parse_sess.span_diagnostic.cm;
+    let expr_span_string = cm.span_to_snippet(expr.span).unwrap();
+
+    let sess = parse::new_parse_sess();
+    let fm = parse::string_to_filemap(&sess, expr_span_string, "<stdin>".to_string());
+    let mut lexer = lexer::StringReader::new(&sess.span_diagnostic, fm);
+
+    let mut expr_to_string = String::new();
+
+    loop {
+        let next = lexer.next_token();
+
+        let string = sess.span_diagnostic.cm.span_to_snippet(next.sp).unwrap();
+
+        let stringified = match next.tok {
+            token::Eof => break,
+            token::Comment => {
+                format!("/* {} */", string.trim_left_chars('/').trim())
+            },
+            _ => {
+                string
+            }
+        };
+
+        expr_to_string.push_str(&*stringified);
+    }
+
+    let mut expr_span_string = String::new();
+    for (i, l) in expr_to_string.lines().enumerate() {
+        if i != 0 {
+            expr_span_string.push(' ');
+            expr_span_string.push_str(l.trim());
+        } else {
+            expr_span_string.push_str(l.trim());
+        }
+    }
+    let expr_to_string_expr =
+            cx.expr_str(expr.span, token::intern_and_get_ident(expr_span_string.trim()));
+    
+    match expr.node {
+        ast::ExprBinary(oper, ref given, ref expected) => {
+            let expr_left = expand_assert_ng_helper(cx, given);
+            let expr_right = expand_assert_ng_helper(cx, expected);
+
+            let if_expr = match oper {
+                ast::BiEq => 
+                    quote_expr!(cx, if !(*given_val == *expected_val) {
+                        println!("assertion failed: {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    }),
+                ast::BiNe => 
+                    quote_expr!(cx, if !(*given_val != *expected_val) {
+                        println!("assertion failed: {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    }),
+                ast::BiGt => 
+                    quote_expr!(cx, if !(*given_val > *expected_val){
+                        println!("assertion failed: {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    }),
+                ast::BiLt => 
+                    quote_expr!(cx, if !(*given_val < *expected_val){
+                        println!("assertion failed: {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    }),
+                ast::BiGe => 
+                    quote_expr!(cx, if !(*given_val >= *expected_val) {
+                        println!("assertion failed: {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    }),
+                ast::BiLe => 
+                    quote_expr!(cx, if !(*given_val <= *expected_val) {
+                        println!("assertion failed: {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    }),
+                _ => {
+                    quote_expr!(cx,
+                        println!("Expr {}:\n\
+                            left:  `{}`\n\
+                            right: `{}`",
+                           $expr_to_string_expr,
+                           *given_val,
+                           *expected_val);
+                    )
+                }
+            };
+            quote_expr!(cx,
+                match (&($given), &($expected)) {
+                    (given_val, expected_val) => {
+                        $if_expr;
+                        $expr_left;
+                        $expr_right;
+                    }
+                }
+            )
+        },
+        ast::ExprParen(ref given) => {
+            expand_assert_ng_helper(cx, given)
+        }
+        _ => {
+            quote_expr!(cx, {})
+        }
+    }
 }
 
 fn expand_assert_ng_(cx: &mut ExtCtxt, _: Span, args: &[TokenTree], debug_only: bool)
@@ -60,156 +190,18 @@ fn expand_assert_ng_(cx: &mut ExtCtxt, _: Span, args: &[TokenTree], debug_only: 
                     span: span,
                 }), None))
     } else {
-        let cm = &cx.parse_sess.span_diagnostic.cm;
-        let expr_span_string = cm.span_to_snippet(expr.span).unwrap();
-
-        let sess = parse::new_parse_sess();
-        let fm = parse::string_to_filemap(&sess, expr_span_string, "<stdin>".to_string());
-        let mut lexer = lexer::StringReader::new(&sess.span_diagnostic, fm);
-
-        let mut expr_to_string = String::new();
-
-        loop {
-            let next = lexer.next_token();
-
-            let string = sess.span_diagnostic.cm.span_to_snippet(next.sp).unwrap();
-
-            let stringified = match next.tok {
-                token::Eof => break,
-                token::Comment => {
-                    format!("/* {} */", string.trim_left_chars('/').trim())
-                },
-                _ => {
-                    string
-                }
-            };
-
-            expr_to_string.push_str(&*stringified);
-        }
-
-        let mut expr_span_string = String::new();
-        for (i, l) in expr_to_string.lines().enumerate() {
-            if i != 0 {
-                expr_span_string.push(' ');
-                expr_span_string.push_str(l.trim());
-            } else {
-                expr_span_string.push_str(l.trim());
-            }
-        }
-
-        let expr_to_string_expr =
-            cx.expr_str(expr.span, token::intern_and_get_ident(expr_span_string.trim()));
-        match expr.node {
-            ast::ExprBinary(ast::BiEq, ref given, ref expected) => {
-                quote_expr!(cx,
-                    match (&($given), &($expected)) {
-                        (given_val, expected_val) => {
-                            if !(*given_val == *expected_val) {
-                                panic!("assertion failed: {}:\n\
-                                        left:  `{}`\n\
-                                        right: `{}`",
-                                       $expr_to_string_expr,
-                                       *given_val,
-                                       *expected_val);
-                            }
-                        }
-                    }
-                )
-            },
-            ast::ExprBinary(ast::BiNe, ref given, ref expected) => {
-                quote_expr!(cx,
-                    match (&($given), &($expected)) {
-                        (given_val, expected_val) => {
-                            if !(*given_val != *expected_val) {
-                                panic!("assertion failed: {}:\n\
-                                        left:  `{}`\n\
-                                        right: `{}`",
-                                       $expr_to_string_expr,
-                                       *given_val,
-                                       *expected_val);
-                            }
-                        }
-                    }
-                )
-            },
-            ast::ExprBinary(ast::BiGt, ref given, ref expected) => {
-                quote_expr!(cx,
-                    match (&($given), &($expected)) {
-                        (given_val, expected_val) => {
-                            if !(*given_val > *expected_val) {
-                                panic!("assertion failed: {}:\n\
-                                        left:  `{}`\n\
-                                        right: `{}`",
-                                       $expr_to_string_expr,
-                                       *given_val,
-                                       *expected_val);
-                            }
-                        }
-                    }
-                )
-            },
-            ast::ExprBinary(ast::BiLt, ref given, ref expected) => {
-                quote_expr!(cx,
-                    match (&($given), &($expected)) {
-                        (given_val, expected_val) => {
-                            if !(*given_val < *expected_val) {
-                                panic!("assertion failed: {}:\n\
-                                        left:  `{}`\n\
-                                        right: `{}`",
-                                       $expr_to_string_expr,
-                                       *given_val,
-                                       *expected_val);
-                            }
-                        }
-                    }
-                )
-            },
-            ast::ExprBinary(ast::BiGe, ref given, ref expected) => {
-                quote_expr!(cx,
-                    match (&($given), &($expected)) {
-                        (given_val, expected_val) => {
-                            if !(*given_val >= *expected_val) {
-                                panic!("assertion failed: {}:\n\
-                                        left:  `{}`\n\
-                                        right: `{}`",
-                                       $expr_to_string_expr,
-                                       *given_val,
-                                       *expected_val);
-                            }
-                        }
-                    }
-                )
-            },
-            ast::ExprBinary(ast::BiLe, ref given, ref expected) => {
-                quote_expr!(cx,
-                    match (&($given), &($expected)) {
-                        (given_val, expected_val) => {
-                            if !(*given_val <= *expected_val) {
-                                panic!("assertion failed: {}:\n\
-                                        left:  `{}`\n\
-                                        right: `{}`",
-                                       $expr_to_string_expr,
-                                       *given_val,
-                                       *expected_val);
-                            }
-                        }
-                    }
-                )
-            },
-            _ => {
-                quote_expr!(cx,
-                    if !($expr) {
-                        panic!(concat!("assertion failed: ", $expr_to_string_expr));
-                    }
-                )
-            }
-        }
+        expand_assert_ng_helper(cx, &expr)
     };
 
     MacExpr::new(if debug_only {
-        quote_expr!(cx, if cfg!(not(ndebug)) { $res })
+        quote_expr!(cx, if cfg!(not(ndebug)) {
+            $res
+        })
     } else {
-        res
+        quote_expr!(cx,
+            $res;
+            panic!("");
+        })
     })
 }
 
